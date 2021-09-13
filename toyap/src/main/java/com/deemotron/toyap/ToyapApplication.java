b632cc467094b4daaa6caa56ad14ab704c0e4313
ashdolldesert <feather.wizard@gmail.com>
2021-09-13 21:13:20 +0800

    topapp - 1-2. Spring Initializr: Create a Maven Project

---
+++ toyap/src/main/java/com/deemotron/toyap/ToyapApplication.java
@@ -0,0 +1,13 @@
+package com.deemotron.toyap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyapApplication.class, args);
+	}
+
+}
---
+++ toyap/src/test/java/com/deemotron/toyap/ToyapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deemotron.toyap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
