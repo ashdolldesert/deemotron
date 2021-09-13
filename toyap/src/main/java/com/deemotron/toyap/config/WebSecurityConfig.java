f4f76b56826e68eeb2f9f7bf6e88971ce61c825e
ashdolldesert <feather.wizard@gmail.com>
2021-09-13 21:17:39 +0800

    topapp - 1-8. Add WebSecurityConfig

---
+++ toyap/src/main/java/com/deemotron/toyap/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.deemotron.toyap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // private static final String[] AUTH_WHITELIST = {
+    //     "/swagger-resources/**",
+    //     "/swagger-ui.html",
+    //     "/v2/api-docs",
+    //     "/webjars/**"
+    // };
+
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and().authorizeRequests()
+                .anyRequest().permitAll()
+                .and().csrf().disable();
+    }
+}
